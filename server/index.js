3396ba3e1ec0306ec7c0a790e33e3b624072583f
Cody Price <CodyWilliamPrice@outlook.com>
2024-04-17 21:10:46 -0400

    Added products and transations to server

--- server/index.js
+++ server/index.js
@@ -6,8 +6,12 @@ import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import kpiRoutes from './routes/kpi.js';
+import productRoutes from './routes/product.js';
+import transactionRoutes from './routes/transaction.js';
 import KPI from './models/KPI.js';
-import { kpis } from './data/data.js';
+import Product from './models/Product.js';
+import Transaction from './models/Transaction.js';
+import { kpis, products, transactions } from './data/data.js';
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -18,10 +22,12 @@ app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors());
 
 /* ROUTES */
 app.use('/kpi', kpiRoutes);
+app.use('/product', productRoutes);
+app.use('/transaction', transactionRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
@@ -36,5 +42,7 @@ mongoose
 		/* ADD DATA ONE TIME ONLY OR AS NEEDED */
 		// await mongoose.connection.db.dropDatabase();
 		// KPI.insertMany(kpis);
+		// Product.insertMany(products);
+		// Transaction.insertMany(transactions);
 	})
 	.catch((error) => console.log(`${error} did not connect`));
